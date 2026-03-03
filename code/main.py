36c858fc2cbb6d248bd1c9a55d78d489ae014428
Carlos Melero <carlos@carlosmelero.com>
2026-03-03 17:17:58 +0100

    Pinta celdas con dobles paredes. Abre muros.

--- code/main.py
+++ code/main.py
@@ -39,6 +39,22 @@ class Wall(IntFlag):
 
 
 class Cell():
+
+    # Relative position of a cell according to N/E/S/W
+    relative: dict[int, tuple[int, int]] = {
+        Wall.N: (-1, 0),
+        Wall.E: (0, +1),
+        Wall.S: (+1, 0),
+        Wall.W: (0, -1)
+        }
+
+    oposite_wall: dict[int, int] = {
+        Wall.N: Wall.S,
+        Wall.E: Wall.W,
+        Wall.S: Wall.N,
+        Wall.W: Wall.E
+        }
+
     def __init__(self, row: int, col: int, maze: "Maze"):
 
         self.maze = maze
@@ -50,7 +66,7 @@ class Cell():
         # Walls. Byte OR of 0001, 0010, 0100 and 1000
         self.walls: int = Wall.N | Wall.E | Wall.S | Wall.W
 
-    def get_neighbor(self) -> list["Cell"]:
+    def get_neighbors(self) -> list["Cell"]:
         neighbors: list["Cell"] = []
         matrix = self.maze.matrix
 
@@ -65,6 +81,17 @@ class Cell():
 
         return (neighbors)
 
+    def open_wall(self, wall: int) -> None:
+
+        self.walls &= ~wall  # Open the wall (and not wall.X)
+
+        neighbor_row = self.row + Cell.relative[wall][0]
+        neighbor_col = self.col + Cell.relative[wall][1]
+
+        if (self.maze.cell_exist(neighbor_row, neighbor_col)):
+            self.maze.matrix[neighbor_row][neighbor_col].walls\
+                &= ~self.oposite_wall[wall]
+
 
 class Maze():
 
@@ -78,11 +105,54 @@ class Maze():
                 for row in range(rows)])
 
     def show(self):
-        for row in self.matrix:
-            print("row ", end="")
-            for cell in row:
-                print(f"[{cell.row}{'X' if cell.visited else 'o'}{cell.col}]", end="")
-            print()
+
+        print("|" + "-----" * self.cols + "|")
+
+        for r in range(self.rows):
+
+            line = "|"
+            for c in range(self.cols):
+                line += "+"
+                cell: Cell = self.matrix[r][c]
+                if cell.walls & Wall.N:  # I'll see double walls, for check
+                    line += "---"
+                else:
+                    line += "   "
+                line += "+"
+            line += "|"
+            print(line) 
+
+            line = "|"
+            for c in range(self.cols):
+                cell: Cell = self.matrix[r][c]
+                if cell.walls & Wall.W:  # I'll see double walls, for check
+                    line += "|"
+                else:
+                    line += " "
+                line += "   "
+                if cell.walls & Wall.E:
+                    line += "|"
+                else:
+                    line += " "
+            line += "|"
+            print(line)
+
+            line = "|"
+            for c in range(self.cols):
+                line += "+"
+                cell: Cell = self.matrix[r][c]
+                if cell.walls & Wall.S:  # I'll see double walls, for check
+                    line += "---"
+                else:
+                    line += "   "
+                line += "+"
+            line += "|"
+            print(line)
+
+        print("|" + "-----" * self.cols + "|")
+
+    def cell_exist(self, row: int, col: int):
+        return (row >= 0 and col >= 0 and row < self.rows and col < self.cols)
 
 
 def pruebavecinos(maze, row, col):
@@ -90,15 +160,45 @@ def pruebavecinos(maze, row, col):
     print(f"\nVecinos de {row} - {col}")
     print("Celda:" + str(m[row][col].row) + " - " + str(m[row][col].col))
 
-    for row in [m[row][col].get_neighbor()]:
+    for row in [m[row][col].get_neighbors()]:
         print("vecinos ", end="")
         for cell in row:
             print(f"[{cell.row}{"X" if cell.visited else "-"}{cell.col}]", end="")
         print()
 
 
-mz = Maze(5, 5)
+def abrecierra(maze: Maze, row: int, col: int):
+
+    my_cell: Cell = maze.matrix[row][col]
+    print("="*20 + "Abre-Cierra" + "="*20)
+    print(f"CELDA: {row}, {col}")
+    maze.show()
+    print(format(my_cell.walls, "04b"))
+    print("Abro N")
+    my_cell.open_wall(Wall.N)
+    maze.show()
+    print("Abro S")
+    my_cell.open_wall(Wall.S)
+    maze.show()
+    print("Abro E")
+    my_cell.open_wall(Wall.E)
+    maze.show()
+    print("Abro W")
+    my_cell.open_wall(Wall.W)
+    maze.show()
+
+
+
+
+mz = Maze(3, 3)
 mz.show()
-pruebavecinos(mz, 0, 0)
-pruebavecinos(mz, 2, 2)
-pruebavecinos(mz, 4, 4)
+# pruebavecinos(mz, 0, 0)
+# pruebavecinos(mz, 2, 2)
+# pruebavecinos(mz, 4, 4)
+
+# abrecierra(mz, 0, 0)
+
+abrecierra(mz, 2, 2)
+mz = Maze(3, 3)
+abrecierra(mz, 1, 1)
+# abrecierra(mz, 4, 4)
